cbeeeba01b470879d8803e1a770d3c6db67ccbee
raul <2048339+raulhaag@users.noreply.github.com>
2022-05-22 12:25:07 -0300

    correct file

--- HttpServer.py
+++ HttpServer.py
@@ -1,5 +1,8 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 import os
+import requests
+
+from requests.api import request
 from actions import getRedirectPost, getResponseGet, getResponsePost
 
 serve = None
@@ -55,14 +58,29 @@ class handler(SimpleHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(message)
 
+def check_for_update():
+    
+    c_version = open("version", "r", encoding="utf-8").read().strip().split(".")
+    c_version = [int(x) for x in c_version]
+    c_version = c_version[0] * 100000000 + c_version[1] * 100000 + c_version[2]
+    r_version = requests.get("https://raw.githubusercontent.com/raulhaag/gean/master/version").text.strip().split(".")
+    r_version = [int(x) for x in r_version]
+    r_version = r_version[0] * 100000000 + r_version[1] * 100000 + r_version[2]
+    if c_version < r_version:
+        print("Update available")
+        print("Current version: " + str(c_version))
+        print("New version: " + str(r_version))
+
 server = HTTPServer(('', 8080), handler)
 
 try:
     try:
-        os.system(r"load.html")
+        #os.system(r"load.html")
+        pass
     except Exception as e:
         print(e)
+    check_for_update()
     server.serve_forever()
-except:
-    pass
+except Exception as e:
+    print(e)
 
