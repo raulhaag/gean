a29dbaf21d3dd210d20f582903b410a5cfaf1888
raul <2048339+raulhaag@users.noreply.github.com>
2022-06-09 08:50:56 -0300

    tioanime 2

--- www/js/sources/sources.js
+++ www/js/sources/sources.js
@@ -1,12 +1,13 @@
 import { JKAnime } from "./jkanime.js";
 import{SoloLatino} from "./sololatino.js";
+import { TioAnime } from "./tioanime.js";
 export function openInNewTab(url) {
     window.open(url, '_blank').focus();
 }
 
-let servers = {"jkanime": new JKAnime(), "sololatino": new SoloLatino()};
+let servers = {"jkanime": new JKAnime(), "sololatino": new SoloLatino(), "tioanime": new TioAnime()};
 
-export function getSource(name) {   return servers[name]; }
+export function getSource(name) {return servers[name];}
 
 export function getResponse(name, callback, error_callback) {
     return servers[name].getFrontPage(callback, error_callback);
---
+++ www/js/sources/tioanime.js
@@ -0,0 +1,124 @@
+export class TioAnime {
+    constructor() {
+      this.name = "tioanime";
+      this.baseUrl = "https://tioanime.com/";
+    }
+    async getFrontPage(after, onError) {
+      let result = await fGet(this.baseUrl);
+      if(result.indexOf("error") == 0){
+        onError(result);
+        return;
+      }
+      var parser = new DOMParser();
+      var doc = parser.parseFromString(result, "text/html");
+      let ncs = [];
+      let flis;
+      try {
+        flis = doc.getElementsByClassName("episode");
+        for (var i = 0; i < flis.length; i++) {
+          ncs.push({
+            "name": flis[i].getElementsByTagName('h3')[0].innerText,
+            "image": this.baseUrl + flis[i].getElementsByTagName('img')[0].getAttribute('src'),
+            "path": this.name + "/getLinks/" + enc(this.baseUrl + flis[i].getElementsByTagName('a')[0].getAttribute("href")),
+          });
+        }
+      } catch (nerror) {
+        onError(nerror);
+      }
+      let nas = [];
+      try {
+        flis = doc.getElementsByClassName("anime");
+        for (var i = 0; i < flis.length; i++) {
+            nas.push({
+                "name": flis[i].getElementsByTagName('h3')[0].innerText,
+                "image": this.baseUrl + flis[i].getElementsByTagName('img')[0].getAttribute('src'),
+                "path": this.name + "/getDescription/" + enc(this.baseUrl + flis[i].getElementsByTagName('a')[0].getAttribute("href")),
+            });
+        }
+      } catch (nerror) {
+        onError(nerror);
+      }
+
+      after({
+        "Últimos Capítulos": ncs,
+        "Últimas Péliculas, Ovas y Animes": nas,
+      });
+    }
+
+    async getDescription(after, onError, path, page = 0, ) {
+      try{
+        let result = await fGet(dec(path));
+        if (result.indexOf("error") == 0) {
+          onError(result);
+          return;
+        }
+        var parser = new DOMParser();
+        var doc = parser.parseFromString(result, "text/html");
+        let sname =  doc.querySelector("h1.title").innerText;
+        let description =  doc.querySelector("p.sinopsis").innerText.trim();
+        let genres = doc.querySelector("p.genres").innerText.trim().replaceAll("\n\n\n", ", ");
+        let image = this.baseUrl + doc.querySelector(".thumb figure img").getAttribute('src');
+        let chaptersIndex = [...result.matchAll(/var episodes = \[([^\]]+)\]/gm)][0][1].split(",")
+        let chapters = [];
+        let basechapter = dec(path).replace("/anime/", "/ver/");
+        for (let i = 1; i < chaptersIndex.length; i++) {
+          chapters.unshift({"name": "Capítulo " + chaptersIndex[i], "path": this.name + "/getLinks/" + window.enc(basechapter + "-" + chaptersIndex[i])});
+        }
+        after({"name": sname, "path": this.name + "/getDescription/" + path, "image": image, "items":[description, genres], "chapters": chapters});
+      } catch (error) {
+        onError(error);
+      }
+    }
+
+    async getParent(after, path) {
+      //let reduce = function(v){after({"name": v.name, "image": v.image, "path": v.path}, path)};
+      //let dpath = (window.dec(path)).split("/");
+      //this.getDescription(reduce, console.log, window.enc(dpath.slice(0, dpath.length - 2).join("/")));
+    }
+
+    async getList(page, filter = "") {
+
+    }
+
+    async getSearch(after, onError, query) {
+      try{
+        let response = await fGet("https://tioanime.com/directorio?q=" + query);
+        if (response.indexOf("error") == 0) {
+          onError(response);
+          return;
+        }
+        var parser = new DOMParser();
+        var doc = parser.parseFromString(response, "text/html");
+        let nas = [];
+        let flis = doc.getElementsByClassName("anime");
+        for (var i = 0; i < flis.length; i++) {
+            nas.push({
+                "name": flis[i].getElementsByTagName('h3')[0].innerText,
+                "image": this.baseUrl + flis[i].getElementsByTagName('img')[0].getAttribute('src'),
+                "path": this.name + "/getDescription/" + enc(this.baseUrl + flis[i].getElementsByTagName('a')[0].getAttribute("href")),
+            });
+        }
+        after(nas);
+      } catch (error) {
+        onError(error);
+      }
+    }
+
+    async getLinks(after, onError, path) {
+      try{
+        let result = await fGet(dec(path));
+        if (result.indexOf("error") == 0) {
+          onError(result);
+          return;
+        }
+        let fames = [...result.matchAll(/\["[^"]+","([^"]+)",.+?,.+?\]/gm)]
+        let links = [];
+        for (let i = 0; i < fames.length; i++) {
+            links.push((fames[i][1]).replaceAll('\\', ""));
+        }
+        after(links);
+      } catch (error) {
+        onError(error);
+      }
+    }
+  }
