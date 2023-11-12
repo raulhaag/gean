4dc82c432738c89ba7376ec4946dc43da024c5f7
raulhaag <2048339+raulhaag@users.noreply.github.com>
2023-11-12 20:07:20 -0300

    0.0.103

--- www/js/vservers/plusvip.js
+++ www/js/vservers/plusvip.js
@@ -3,14 +3,14 @@ export class Plusvip {
     async getDDL(after, onError, web){
         try{
             var link = web.split("data=")[1]; 
-            var data = await fGet(web);
+            var data = await fGet(web, {"User-Agent": navigator.userAgent});
             var parser = new DOMParser();
             var doc = parser.parseFromString(data, "text/html");
-            var u = doc.getElementById("cuca").text.replace('"', '');
-            var R = CryptoJS.enc.Utf8.parse(dec('ZDQxZDhjZDk4ZjAwYjIwNGU5ODAwOTk4ZWNmODQyN2U='));
+            var u = JSON.parse(doc.getElementById("cuca").text);
+            var R = CryptoJS.enc.Utf8.parse('d41d8cd98f00b204e9800998ecf8427e');
             var N = CryptoJS.AES.decrypt(u, R, { mode: CryptoJS.mode.ECB });
             var web2 = "https://plusvip.net" + N.toString(CryptoJS.enc.Utf8);
-            var webC1 = await fPost(web2, {"Referer":web}, {"link":link});
+            var webC1 = await fPost(web2, {"User-Agent":navigator.userAgent}, {"link":link});
             after({video: JSON.parse(webC1).link});
         }catch(error){
             onError(error);
