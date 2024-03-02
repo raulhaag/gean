fa7838f2126a9eecae1f2ea43d4205d1b29b4bea
raulhaag <2048339+raulhaag@users.noreply.github.com>
2024-03-02 11:58:56 -0300

    dev test2

--- www/js/vservers/plustr.js
+++ www/js/vservers/plustr.js
@@ -5,8 +5,8 @@ export class Plustr {
             try{
                 var data = await fGet(web, {"User-Agent": navigator.userAgent, Referer:'https://streamsito.com/'});
                 eval(atob('ICAgIHZhciBzdHJlYW0gPSBnZXRGaXJzdE1hdGNoKC9bPSxdJyhbXiddezIwMCwxMDAwfSkvZ20sIGRhdGEpOwogICAgdmFyIHBhc3MgID0gZ2V0Rmlyc3RNYXRjaCgvLCcoW14nXlxcXXszMn0pJy9nbSwgZGF0YSk7CiAgICB2YXIgTjIgPSBDcnlwdG9KUy5BRVMuZGVjcnlwdChzdHJlYW0sIHBhc3MpOwogICAgd2luZG93LnR3ID0gKE4yLnRvU3RyaW5nKENyeXB0b0pTLmVuYy5VdGY4KSk7Cg=='), "decode");
-                var data2 = await fGet(tw);
-                
+                //var data2 = await fGet(tw);
+
                 
                 after({video: window.serverHost + "file/" + enc(tw) + "/" + enc(JSON.stringify({}))});
             }catch(error){
