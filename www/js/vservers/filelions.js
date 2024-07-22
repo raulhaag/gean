4d486b39f23f2086a6e3b469128e8cc9eae10d41
raul <raul@home.com>
2024-07-22 18:17:38 -0300

    flions

--- www/js/vservers/filelions.js
+++ www/js/vservers/filelions.js
@@ -1,6 +1,25 @@
-import { FileMoon } from "./filemoon.js";
-export class FileLions extends FileMoon{
-    constructor() {
-        super();
+export class FileLions{
+    constructor() {}
+    async getDDL(after, onError, web){
+        try{
+            let content = await fGet(web);
+            var match = getFirstMatch(/(eval\(function\(p,a,c,k,e,d\)[\S\s]+?\.split\('\|'\)\)\))/gm, content);
+            if (match) {
+                var funcionDesofuscada = match.replace('eval', 'return');
+                var desofuscado = new Function(funcionDesofuscada);
+                var data = desofuscado();
+                var dlink = getFirstMatch(/file:"(.+?)"/gm, data);
+                after({"video":dlink});
+            }else{
+                let dlink = getFirstMatch(/file:"(.+?)"/gm, content);
+                if(dlink != ""){
+                    after({"video": dlink});
+                }else{
+                    onError("No se encontro el enlace");
+                }
+            }
+        }catch(error){
+            onError(error);
+        }
     }
 }
