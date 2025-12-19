c230e12b71275cf7ff2b378285812e06c0eb43a4
raul <raul@home>
2025-12-19 11:00:01 -0300

    232

--- www/2js/scene_search.js
+++ www/2js/scene_search.js
@@ -5,9 +5,19 @@ export class SceneSearch extends Scene{
     lastkey = null;
     lastMedia = null;
     searchtext = null;
+    lastSH = null;
     constructor(){
         super(true);
         this.lastKeyManager = this.keys_nav;
+    }
+
+    moveFocus(oldElement, newElement) {
+        if(newElement) {
+            if(oldElement) oldElement.classList.remove("focus");
+            newElement.classList.add("focus");
+            return newElement;
+        }
+        return null;
     }
 
     keys_nav(event){
@@ -26,9 +36,7 @@ export class SceneSearch extends Scene{
                 }
                 newselection = document.getElementById("search-box_" + (cr-1) + "_" + cc);
                 if(newselection){
-                    this.lastkey.classList.remove("focus");
-                    this.lastkey = newselection;
-                    this.lastkey.classList.add("focus");
+                    this.lastkey = this.moveFocus(this.lastkey, newselection);
                 }
                 break;
             case 'ArrowDown':
@@ -37,18 +45,20 @@ export class SceneSearch extends Scene{
                 }
                 newselection = document.getElementById("search-box_" + (cr+1) + "_" + cc);
                 if(newselection){
-                    this.lastkey.classList.remove("focus");
-                    this.lastkey = newselection;
-                    this.lastkey.classList.add("focus");
+                    this.lastkey = this.moveFocus(this.lastkey, newselection);
+                }else{
+                    if (this.lastSH){
+                        this.lastKeyManager = this.search_history_nav;
+                        this.lastkey.classList.remove("focus");
+                        this.lastKeyManager(null);
+                    }
                 }
                 break;
 
             case 'ArrowLeft':
                     newselection = document.getElementById("search-box_" + cr + "_" + (cc - 1));
                     if(newselection){
-                        this.lastkey.classList.remove("focus");
-                        this.lastkey = newselection;
-                        this.lastkey.classList.add("focus");
+                        this.lastkey = this.moveFocus(this.lastkey, newselection);
                     }
                     if(cc === 1){
                         tease_menu(true);
@@ -61,9 +71,7 @@ export class SceneSearch extends Scene{
             case 'ArrowRight':
                 newselection = document.getElementById("search-box_" + cr + "_" + (cc + 1));
                 if(newselection){
-                    this.lastkey.classList.remove("focus");
-                    this.lastkey = newselection;
-                    this.lastkey.classList.add("focus");
+                    this.lastkey = this.moveFocus(this.lastkey, newselection);
                 }else{
                     if(this.lastMedia != null){
                         this.lastKeyManager = this.search_result_nav;
@@ -88,9 +96,14 @@ export class SceneSearch extends Scene{
                         this.searchtext.innerHTML = this.searchtext.innerHTML + " ";
                         break;
                     case '✓':
-                        showLoading();
-                        let server = getSource(sid);
-                        server.getSearch((items) =>{this.searchDone(items, this)}, error, this.searchtext.innerHTML.replace(' ', '+'))
+                        let st = this.searchtext.innerHTML.replace(' ', '+');
+                        if (st.length > 0){
+                            showLoading();
+                            this.updateSearchHistory(st);
+                            this.renderSearchHistory();
+                            let server = getSource(sid);
+                            server.getSearch((items) =>{this.searchDone(items, this)}, error, st)
+                        }
                         break;
                     default:
                         this.searchtext.innerHTML = this.searchtext.innerHTML + this.lastkey.innerHTML;
@@ -113,6 +126,88 @@ export class SceneSearch extends Scene{
         }
     }
 
+    search_history_nav(event){
+        if(event == null){
+            this.lastSH.classList.add("focus");
+            tease_menu(true);
+            return;
+        }
+        let itempos = this.lastSH.id.split("_").reverse();
+        let cc = parseInt(itempos[0]);
+        let cr = parseInt(itempos[1]);
+        let newselection;
+        switch(event.key){
+            case 'ArrowUp':
+                newselection = document.getElementById("search-history-ph_" + (cr-1) + "_" + cc);
+                if(newselection){
+                    this.lastSH = this.moveFocus(this.lastSH, newselection);
+                    break;
+                }else if(cc != 0){
+                    newselection = document.getElementById("search-history-ph_" + (cr-1) + "_0");
+                    if(newselection) {
+                        this.lastSH = this.moveFocus(this.lastSH, newselection);
+                        break;
+                    }
+                }
+                this.lastKeyManager = this.keys_nav;
+                this.lastSH.classList.remove("focus");
+                this.lastkey.classList.add("focus");
+                break;
+            case 'ArrowDown':
+                newselection = document.getElementById("search-history-ph_" + (cr+1) + "_" + cc);
+                if(newselection){
+                    this.lastSH = this.moveFocus(this.lastSH, newselection);
+                }else if(cc != 0){
+                    newselection = document.getElementById("search-history-ph_" + (cr+1) + "_0");
+                    if(newselection) this.lastSH = this.moveFocus(this.lastSH, newselection);
+                }
+                break;
+            case 'ArrowLeft':
+                newselection = document.getElementById("search-history-ph_" + cr + "_" + (cc - 1));
+                if(newselection){
+                    this.lastSH = this.moveFocus(this.lastSH, newselection);
+                }
+                if(cc === 1){
+                    tease_menu(true);
+                }else if(cc === 0){
+                    this.lastSH.classList.remove("focus");
+                    menuManager();
+                }
+                break;
+            case 'ArrowRight':
+                newselection = document.getElementById("search-history-ph_" + cr + "_" + (cc + 1));
+                if(newselection){
+                    this.lastSH = this.moveFocus(this.lastSH, newselection);
+                    if(cc === 0) tease_menu(false);
+                }else{
+                    if(this.lastMedia != null){
+                        this.lastKeyManager = this.search_result_nav;
+                        this.lastSH.classList.remove("focus");
+                        this.lastMedia.classList.add("focus");
+                        tease_menu(false);
+                    }
+                }
+                break;
+            case "Enter":
+            case "NumpadEnter":
+            case 'Space':
+            case ' ':
+                let query = this.lastSH.dataset.query;
+                if (query){
+                    showLoading();
+                    this.searchtext.innerHTML = query;
+                    let server = getSource(sid);
+                    server.getSearch((items) =>{this.searchDone(items, this)}, error, query)
+                    this.lastSH.classList.remove("focus");
+                    this.lastSH = document.getElementById("search-history-ph_0_0");
+                    this.lastkey.classList.remove("focus");
+                    this.lastkey = document.getElementById("search-box_0_2");
+                    tease_menu(false);
+                }
+                break;
+        }
+    }
+
     search_result_nav(event){
         if(event == null){
             this.lastMedia.classList.add("focus");
@@ -126,40 +221,32 @@ export class SceneSearch extends Scene{
             case 'ArrowUp':
                 newselection = document.getElementById("search-results-ph_" + (cr-1) + "_" + cc);
                 if(newselection){
-                    this.lastMedia.classList.remove("focus");
-                    this.lastMedia = newselection;
-                    this.lastMedia.classList.add("focus");
+                    this.lastMedia = this.moveFocus(this.lastMedia, newselection);
                     this.lastMedia.parentElement.scrollTop = this.lastMedia.offsetTop - 65;
                 }
                 break;
             case 'ArrowDown':
                 newselection = document.getElementById("search-results-ph_" + (cr+1) + "_" + cc);
                 if(newselection){
-                    this.lastMedia.classList.remove("focus");
-                    this.lastMedia = newselection;
-                    this.lastMedia.classList.add("focus");
+                    this.lastMedia = this.moveFocus(this.lastMedia, newselection);
                     this.lastMedia.parentElement.scrollTop = this.lastMedia.offsetTop - 65;
                 }
                 break;
             case 'ArrowLeft':
                     newselection = document.getElementById("search-results-ph_" + cr + "_" + (cc - 1));
                     if(newselection){
-                        this.lastMedia.classList.remove("focus");
-                        this.lastMedia = newselection;
-                        this.lastMedia.classList.add("focus");
+                        this.lastMedia = this.moveFocus(this.lastMedia, newselection);
                     }
                     if(cc === 0){
                         this.lastKeyManager = this.keys_nav;
                         this.lastMedia.classList.remove("focus");
-                        this.lastkey.classList.add("focus");
+                        this.lastKeyManager(null);
                     }
                 break;
             case 'ArrowRight':
                 newselection = document.getElementById("search-results-ph_" + cr + "_" + (cc + 1));
                 if(newselection){
-                    this.lastMedia.classList.remove("focus");
-                    this.lastMedia = newselection;
-                    this.lastMedia.classList.add("focus");
+                    this.lastMedia = this.moveFocus(this.lastMedia, newselection);
                 }
                 break;
             case "Enter":
@@ -172,7 +259,9 @@ export class SceneSearch extends Scene{
     }
 
     initBindings(){
-        this.lastkey = this.updatePositionsSr("search-box", this.lastkey);
+        let searchBox = document.getElementsByClassName("search-box")[0];
+        this.lastkey = this.updatePositions(searchBox, "search-box");
+        this.renderSearchHistory();
         this.searchtext = document.getElementsByClassName("search-text")[0];
         hideLoading();
     }
@@ -220,6 +309,7 @@ export class SceneSearch extends Scene{
                         <div class="search-key focusable">9</div>
                         <div class="search-key focusable">0</div>
                     </div>
+                    <div class="search-history-ph" id="search-history-ph"></div>
                 </div>
             <div class="search-results">
                 <div type="text" class="search-text"></div>
@@ -233,14 +323,44 @@ export class SceneSearch extends Scene{
             innerHTML += '<div class="search_item focusable" data-path="' + items[i]["path"] + '"><img class="search_item__image" src="' + items[i]['image'] + '" alt=""> <h2 class="search_item__title">' + items[i]['name'] + '</h2></div>';
         }
         document.getElementById("search-results-ph").innerHTML = innerHTML + '</div></div>'
-        instance.lastMedia = instance.updatePositionsSr("search-results-ph", this.lastMedia);
-        instance.lastKeyManager = instance.search_result_nav;
-        instance.lastkey.classList.remove("focus");
+        let resultsPh = document.getElementById("search-results-ph");
+        instance.lastMedia = instance.updatePositions(resultsPh, "search-results-ph");
+        if (items.length > 0){
+            instance.lastKeyManager = instance.search_result_nav;
+            instance.lastkey.classList.remove("focus");
+        }
         hideLoading();
     }
-    updatePositionsSr(containerCN = "content", lastsel){
-        let container = document.getElementsByClassName(containerCN)[0];
-        let items = container.getElementsByClassName("focusable");//focusable next??
+
+    renderSearchHistory(){
+        let history = JSON.parse(localStorage.getItem("searchHistory") || "[]");
+        if(history.length > 0){
+            let innerHTML = '';
+            for (let i = 0; i < history.length; i++) {
+                innerHTML += `<div class="search-history-item focusable" data-query="${history[i]}">${history[i]}</div>`;
+            }
+            let historyPh = document.getElementById("search-history-ph");
+            historyPh.innerHTML = innerHTML;
+            this.lastSH = this.updatePositions(historyPh, "search-history-ph");
+        }
+    }
+
+    updateSearchHistory(newTerm){
+        if(!newTerm || newTerm.length === 0) return;
+        let history = JSON.parse(localStorage.getItem("searchHistory") || "[]");
+        history = history.filter(item => item !== newTerm);
+        history.unshift(newTerm);
+        if(history.length > 15) {
+            history.pop();
+        }
+        localStorage.setItem("searchHistory", JSON.stringify(history));
+    }
+
+    updatePositions(container, prefix){
+        let items = container.getElementsByClassName("focusable");
+        if (items.length == 0){
+            return null;
+        }
         let ctop = items[0].offsetTop;
         let rc = 0, cc = 0;
         for(let i = 0; i < items.length; i++){
@@ -249,11 +369,9 @@ export class SceneSearch extends Scene{
                 rc++;
                 cc = 0;
             }
-            items[i].id = containerCN + "_" + rc + "_" + cc;
+            items[i].id = prefix + "_" + rc + "_" + cc;
             cc++;
         }
-        lastsel = document.getElementById(containerCN + "_" + 0 + "_" + 0);
-        lastsel.classList.add("focus");
-        return lastsel;
+        return document.getElementById(prefix + "_" + 0 + "_" + 0);
     }
 }
--- www/js/vservers/mfire.js
+++ www/js/vservers/mfire.js
@@ -3,13 +3,17 @@ export class Mediafire {
     async getDDL(after, onError, web){
         try{
             let data = await fGet(web);
-            //let dlink = getFirstMatch(/Download file\"\s+href=\"(.+?)\"/gm, data);
-            let dlink = window.dec(getFirstMatch(/data-scrambled-url="([^"]+)/gm, data));
-            after({video: dlink});
-            return;
+            let dlink = window.getFirstMatch(/Download file\"\s+href=\"(.+?)\"/gm, data);
+            //let dlink = window.dec(getFirstMatch(/data-scrambled-url="([^"]+)/gm, data));
+            if (dlink != "") {
+                after({video: dlink});
+                return;
+            }
             // then recaptcha f***
         }catch(e){
             onError(e);
         }
+        onError("No se encontro el enlace");
+        return;
     }
 }
